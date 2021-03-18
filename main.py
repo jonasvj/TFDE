9e42874c0c4f48d4d9b7e4fdd5ac3652dfc9014c
jonasvj <jonas_vez@hotmail.com>
2021-03-18 23:08:07 +0100

    Change data loading

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # Imports
-import matplotlib.pyplot as plt
-import numpy as np
 import pyro
 import torch
-import toy_data as toy_data
+import numpy as np
+from datasets import load_data
+import matplotlib.pyplot as plt
 from models import GaussianMixtureModel, CPModel, TensorTrain
 from utils import plot_density, plot_density_alt, plot_train_loss
 pyro.enable_validation(True)
@@ -13,12 +13,16 @@ plt.style.use('seaborn-dark')
 np.set_printoptions(precision=3)
 
 # Select dataset and model
-method = 'checkerboard'
+data_type = 'checkerboard'
 model_type = 'TensorTrain'
 K = [2,2,2]
 
-# Generate data
-data = torch.tensor(toy_data.inf_train_gen(method, batch_size=5000), dtype=torch.float)
+# Load data
+data = load_data(data_type)
+data_train = torch.tensor(data.trn.x)
+data_val = torch.tensor(data.val.x)
+data_test = torch.tensor(data.tst.x)
+
 #data = torch.column_stack((data, torch.zeros(5000))) # Test for 3D ---- add 0s as third element
 # Instantiate model
 model = eval(model_type)(K)
@@ -31,13 +35,13 @@ print(f"Total density: {dens.item()}")
 #%% Train the model
 if model_type == 'TensorTrain':
     print("[Initialising tensor train parameters]")
-    model.hot_start(data, n_starts=250)
+    model.hot_start(data_train, n_starts=250)
     print("[Tensor train initialised]")
-model.fit_model(data, n_steps=2000)
+model.fit_model(data_train, n_steps=250)
 
 #%%
 # Log likelihood of data
-print(f'Log likelihood of data: {-model.log_likelihood(data).item():.4f}')
+print(f'Negative log likelihood of data: {-model.log_likelihood(data_train).item():.4f}')
 
 #%%
 # Plot train loss
@@ -46,7 +50,7 @@ plot_train_loss(model)
 #%%
 # Plot density
 #plot_density(model, data)
-plot_density_alt(model, data)
+plot_density_alt(model, data_train)
 
 #%%
 #model.unit_test([(-5, 5), (-5, 5)], opts={'epsabs': 0.01}) # Allow a larger error for faster computation
@@ -64,7 +68,7 @@ with torch.no_grad():
     sampled_data = model().detach().numpy()
 
 fig, ax = plt.subplots(1, 2, figsize=(10, 5))
-ax[0].scatter(x=data[:, 0], y=data[:, 1], color='k', alpha=0.3)
+ax[0].scatter(x=data_train[:, 0], y=data_train[:, 1], color='k', alpha=0.3)
 ax[0].set_title("True data")
 ax[1].scatter(x=sampled_data[:, 0], y=sampled_data[:, 1], color='k', alpha=0.3)
 ax[1].set_title("Generated data")
