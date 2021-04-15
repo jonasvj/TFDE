a63844d9b3e7e0abb0bab6af245d5fee1a3383e6
jonasvj <jonas_vez@hotmail.com>
2021-04-16 00:22:35 +0200

    Set subsample size 0 to all data

--- train_tt.py
+++ train_tt.py
@@ -79,6 +79,9 @@ if __name__ == '__main__':
     args = cli()
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
+    if args.subsample_size == 0:
+        args.subsample_size = None
+
     data = load_data(
         args.dataset, 
         optimal_order=bool(args.optimal_order),
