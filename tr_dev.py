9d798c47dfc55dd4b7de24e1bb0f0c07698c733c
Peter Groth <petermoerchgroth@gmail.com>
2021-04-19 11:21:37 +0200

    Full GMM changes

--- tr_dev.py
+++ tr_dev.py
@@ -1,3 +1,4 @@
+#%%
 # Imports
 import pyro
 import sys
@@ -11,30 +12,38 @@ pyro.enable_validation(True)
 
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print(f'Using device: {device}')
 
+
+#print(torch.cuda.device_count())
+#sys.exit(1)
 # Plot options
 plt.style.use('seaborn-dark')
 np.set_printoptions(precision=3)
 
 # Select dataset and model
 data_type = 'checkerboard'
-model_type = 'TensorRing'
+model_type = 'GaussianMixtureModelFull'
 n_dim = 2 # dimensions of data
-K = [6]*(n_dim + 1)
+K = [8]*(n_dim + 1) # 35 for full GMM = TT with Ks = 6
+
 
 # Load data
 data = load_data(data_type, optimal_order=False)
+
 data_train = torch.tensor(data.trn.x).to(device)
 data_val = torch.tensor(data.val.x).to(device)
 data_test = torch.tensor(data.tst.x).to(device)
 
+data_train = data_train[:1000,:]
 #data = torch.column_stack((data, torch.zeros(5000))) # Test for 3D ---- add 0s as third element
 # Instantiate model
 model = eval(model_type)(K, device=device)
-
+model.init_from_data(data_train, k_means=True)
 
 #%%
-#dens = model.unit_test_multidimensional(np.array([-5, 5]*n_dim).reshape(-1), n_points=10)
+
+#dens = model.unit_test_multidimensional(np.array([-5, 5]*n_dim).reshape(-1), n_points=400)
 #print(f"Total density: {dens.item()}")
 
 
@@ -45,7 +54,7 @@ if model_type in ['TensorTrain', 'TensorRing'] :
     print("[Tensor train/ring initialised]")
 else:
     model.init_from_data(data_train, k_means=True)
-model.fit_model(data_train, mb_size=len(data_train), n_epochs=20000)
+model.fit_model(data_train, mb_size=len(data_train), n_epochs=2000)
 
 #%%
 # Log likelihood of data
@@ -55,4 +64,10 @@ print(f'Negative log likelihood of data: {-model.log_likelihood(data_train).item
 #%%
 # Plot train loss
 #plot_train_loss(model)
-plot_density_alt(model, data_train.detach().cpu().numpy())
+fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(24, 6))
+plot_train_loss(model, axes[0])
+plot_density(model, data_train.detach().cpu().numpy(), 
+			density_grid=[-3, 3, -3, 3],
+			axes=axes[1:])
+
+plt.savefig("tmp.png")
