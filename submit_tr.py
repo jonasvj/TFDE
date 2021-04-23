62d2b7d7a6d894cb709891c1db2e910558b39e5a
jonasvj <jonas_vez@hotmail.com>
2021-04-23 20:57:45 +0200

    Scripts for training and submitting Tensor Ring

---
+++ submit_tr.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+import time
+import subprocess
+from itertools import product
+
+def write_bsub(command, model_name, sys_mem='64GB', hours='05', minutes='00',
+    gpu_queue='gpuv100'):
+
+    bsub_string = \
+        '#!/bin/sh\n' \
+        '### General options\n' \
+        '### –- specify queue --\n' \
+        '#BSUB -q {gpu_queue}\n' \
+        '### -- set the job Name --\n' \
+        '#BSUB -J {job_name}\n' \
+        '### -- ask for number of cores (default: 1) --\n' \
+        '#BSUB -n 1\n' \
+        '### -- Select the resources: 1 gpu in exclusive process mode --\n' \
+        '#BSUB -gpu "num=1:mode=exclusive_process"\n' \
+        '### -- set walltime limit: hh:mm --\n' \
+        '#BSUB -W {hours}:{minutes}\n' \
+        '# request system-memory\n' \
+        '#BSUB -R "rusage[mem={sys_mem}]"\n' \
+        '###BSUB -R "select[gpu32gb]"\n' \
+        '### -- Specify the output and error file. %J is the job-id --\n' \
+        '### -- -o and -e mean append, -oo and -eo mean overwrite --\n' \
+        '#BSUB -o {out_file}\n' \
+        '#BSUB -e {error_file}\n' \
+        '# -- end of LSF options --\n' \
+        '# -- start of user input --\n' \
+        'source ~/.virtualenvs/tfde/bin/activate\n' \
+        'cd ~/TFDE\n' \
+        '{command}\n' \
+        ''.format(
+            gpu_queue=gpu_queue,
+            job_name=model_name,
+            sys_mem=sys_mem,
+            hours=hours,
+            minutes=minutes,
+            out_file='misc_files/' + model_name + '.out',
+            error_file='misc_files/' + model_name + '.err',
+            command=command)
+
+    bsub_file_name = 'misc_files/' + model_name + '.bsub'
+    bsub_file = open(bsub_file_name, 'w')
+    bsub_file.write(bsub_string)
+    bsub_file.close()
+    time.sleep(1)
+
+    return bsub_file_name
+
+if __name__ == '__main__':
+
+    datasets = ['power']
+    K_range = [2, 4, 7, 9, 12, 14, 17, 19, 22, 25]
+    datasets = ['hepmass']
+    K_range = [2, 3, 4, 5, 6, 7, 8, 9, 11 ,13]
+    datasets = ['miniboone']
+    K_range = [2, 3, 4, 5, 6, 7, 8, 9]
+    
+    mini_batch_sizes = [64]
+    learning_rates = [3e-4]
+    n_epochs = [300]
+    subsample_sizes = [28000]
+    optimal_order = [1]
+    n_starts = [500]
+    early_stopping = [1]
+    n_runs = range(3)
+
+    train_time = '10'
+    queue = 'gpuv100'
+
+    all_runs = product(
+        datasets, K_range, mini_batch_sizes, learning_rates, n_epochs, 
+        subsample_sizes, optimal_order, n_starts, early_stopping, n_runs)
+
+    for dataset, K, mb_size, lr, epochs, subsample_size, order, n_start, early_stop, run in all_runs:
+        model_name = 'TR_{}_{}_{}_{}_{}_{}_{}_{}_{}_{}'.format(
+            dataset, K, mb_size, lr, epochs, subsample_size, order, n_start, early_stop, run)
+
+        command = './train_tr.py --dataset {dataset} --K {K} ' \
+            '--mb_size {mb_size} --lr {lr} --epochs {epochs} ' \
+            '--subsample_size {subsample_size} --optimal_order {order} ' \
+            '--n_starts {n_start} --early_stopping {early_stop} {model_name}' \
+            ''.format(
+                dataset=dataset, K=K, mb_size=mb_size, lr=lr, epochs=epochs,
+                subsample_size=subsample_size, order=order, n_start=n_start,
+                early_stop=early_stop, model_name=model_name)
+        
+        bsub_file_name = write_bsub(
+            command, model_name, gpu_queue=queue, hours=train_time)
+    
+        bsub_file = open(bsub_file_name, 'rb')
+        subprocess.run(['bsub'], stdin=bsub_file)
+        bsub_file.close()
---
+++ train_tr.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+import sys
+import time
+import torch
+import argparse
+import numpy as np
+from utils import save_model
+from models import TensorRingAlt
+from datasets import load_data, all_datasets
+
+def cli():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        allow_abbrev=False,
+        description='Script for training Tensor Ring model')
+    
+    parser.add_argument(
+        'model_name',
+        help='Name to save model as.'
+    )
+    parser.add_argument(
+        '--dataset',
+        help='Dataset to fit model on.',
+        choices=all_datasets,
+        default='checkerboard'
+    )
+    parser.add_argument(
+        '--K',
+        help='Choice of K for tensor ring model.',
+        type=int,
+        default=10
+    )
+    parser.add_argument(
+        '--mb_size',
+        help='Mini batch size.',
+        type=int,
+        default=256,
+    )
+    parser.add_argument(
+        '--lr',
+        help='Learning rate.',
+        type=float,
+        default=3e-4,
+    )
+    parser.add_argument(
+        '--epochs',
+        help='Number of training epochs.',
+        type=int,
+        default=500,
+    )
+    parser.add_argument(
+        '--subsample_size',
+        help='Subsample training data.',
+        type=int,
+        default=None
+    )
+    parser.add_argument(
+        '--optimal_order',
+        help='Whether to order attributes (1 for True and 0 for False).',
+        type=int,
+        default=0
+    )
+    parser.add_argument(
+        '--n_starts',
+        help='Number of initializations',
+        type=int,
+        default=500
+    )
+    parser.add_argument(
+        '--early_stopping',
+        help='Whether to use early stopping (1 for True and 0 for False).',
+        type=int,
+        default=0
+    )
+
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    args = cli()
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    if args.subsample_size == 0:
+        args.subsample_size = None
+
+    data = load_data(
+        args.dataset, 
+        optimal_order=bool(args.optimal_order),
+        subsample_size=args.subsample_size,
+        tr=True)
+    
+    data_train = torch.tensor(data.trn.x).to(device)
+    data_val = torch.tensor(data.val.x).to(device)
+    del data
+
+    train_start = time.time()
+
+    Ks = [args.K]*(data_train.shape[1])
+    model = TensorRingAlt(Ks=Ks, device=device)
+    model.hot_start(
+        data_train, subsample_size=args.subsample_size, n_starts=args.n_starts)
+    model.fit_model(
+        data_train, data_val=data_val, mb_size=args.mb_size,
+        n_epochs=args.epochs, lr=args.lr,
+        early_stopping=bool(args.early_stopping))
+
+    train_end = time.time()
+    print('Training time: {:.1f} seconds'.format(train_end-train_start))
+    
+    save_model(model, args.model_name)
